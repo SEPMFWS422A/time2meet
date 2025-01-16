b82fe1edbada37239a02e3b08dd9e90f756795a4
Christian <Christian.Huwendiek@edu.fhdw.de>
2025-01-16 14:46:04 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
 
-export default function DownloadICS() {
+export default function Home() {
     const downloadICS = () => {
         const link = document.createElement("a");
         link.href = "/api/event";
@@ -34,7 +34,7 @@ export default function DownloadICS() {
   ];
 
   return (
-    <div  id="HomePageLayout"  className="grid grid-rows-[] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div  id="HomePageLayout"  className="min-h-screen">
     <div className="ml-8 mr-8">
       <FullCalendar height={"100vh"}
         plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin,listPlugin]}
@@ -45,7 +45,7 @@ export default function DownloadICS() {
         initialView="dayGridMonth"
       />
     </div>
-    <div id="DownloadDivCalendar">
+    <div id="DownloadDivCalendar" className="mt-4 mb-4 flex items-center justify-center">
     <button id="Calendardownload" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={downloadICS}>Kalender-Eintrag herunterladen</button>
   </div>
   </div>
