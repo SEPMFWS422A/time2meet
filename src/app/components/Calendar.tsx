6a1454eec5f9f75eda4694741f83efadc6f947e3
elmarraki88 <110703630+elmarraki88@users.noreply.github.com>
2025-01-16 19:48:27 +0100

    #6 Refactoring Calendar
    Create Calendar Component and implement it in page.tsx

---
+++ src/app/components/Calendar.tsx
@@ -0,0 +1,61 @@
+import React, { useRef } from "react";
+import FullCalendar from "@fullcalendar/react";
+import { EventClickArg } from "@fullcalendar/core/index.js";
+import { DateClickArg } from "@fullcalendar/interaction/index.js";
+import { EventInput } from "@fullcalendar/core/index.js";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+
+interface CalendarProps {
+  events: EventInput[];
+}
+
+const Calendar: React.FC<CalendarProps> = ({ events }) => {
+  const calendarRef = useRef<FullCalendar>(null);
+
+  //Handler für Date-klick
+  const handleDateClick = (info: any) => {
+    alert("Datum geklickt: ${info.dateStr}");
+  };
+
+  // Handle für Event-Klick
+  const handleEventClick = (info: any) => {
+    alert("Event geklickt: ${info.event.title}");
+  };
+
+  return (
+    <div>
+      <FullCalendar
+        ref={calendarRef}
+        height={"100vh"}
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
+        }}
+        initialView="dayGridMonth"
+        selectable={true}
+        editable={true}
+        events={events}
+        eventContent={renderEventContent}
+        dateClick={handleDateClick}
+        eventClick={handleEventClick}
+        eventColor="green"
+      />
+    </div>
+  );
+};
+
+function renderEventContent(eventInfo: any) {
+  return (
+    <div>
+      <strong>{eventInfo.event.title}</strong>
+      <p>{eventInfo.event.extendedProps.description}</p>
+    </div>
+  );
+}
+
+export default Calendar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,60 +5,59 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
+import dynamic from "next/dynamic";
+import { EventInput } from "@fullcalendar/core/index.js";
+import Calendar from "@/app/components/Calendar";
 
 export default function Home() {
-    const downloadICS = () => {
-        const link = document.createElement("a");
-        link.href = "/api/event";
-        link.download = "event.ics";
-        link.click();
-    };
-
-  const handleDateClick = (arg: any) => {
-    console.log(arg)
-    alert(arg);
+  const downloadICS = () => {
+    const link = document.createElement("a");
+    link.href = "/api/event";
+    link.download = "event.ics";
+    link.click();
   };
-  const exampleEvents = [
+
+  const events: EventInput[] = [
     {
-      title: "Event 1",
-      start: new Date(2025,0,21).toISOString().split("T")[0],
-      end: new Date(2025,0,24).toISOString().split("T")[0],
-      description: "Termin beim Zahnarzt um 17:00 Uhr",
+      title: "event1",
+      start: "2025-01-17T10:00:00",
+      end: "2025-01-17T12:00:00",
+      description: "event 1 description cdwbgcjkhb cwdhbcjgb cwhbjzgbcw",
+      backgroundColor: "green",
     },
     {
-      title: "Event 2",
-      start: "20250116T130000",
-      duration: { hours: 1, minutes: 30 },
-      description: "Termin beim Zahnarzt um 14:00 Uhr",
+      title: "event2",
+      start: "2025-01-20",
+      allDay: true,
+      description: "event 2 description",
+      backgroundColor: "blue",
+    },
+    {
+      title: "event3",
+      start: "2025-01-22T08:00:00",
+      end: "2025-01-23T18:00:00",
+      description: "event 3 description",
     },
   ];
 
   return (
-    <div  id="HomePageLayout"  className="min-h-screen">
-    <div className="ml-8 mr-8">
-      <FullCalendar height={"100vh"}
-        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin,listPlugin]}
-        headerToolbar ={{left: "prev,next today", center:"title", right:"dayGridMonth,timeGridWeek,timeGridDay,listWeek"}}
-        dateClick={handleDateClick}
-        events={exampleEvents}
-        eventContent={renderEventContent}
-        initialView="dayGridMonth"
-      />
+    <div id="HomePageLayout" className="min-h-screen">
+      <h1>time2meet Kalender</h1>
+      <div className="ml-8 mr-8">
+        <Calendar events={events} />
+      </div>
+      <div
+        id="DownloadDivCalendar"
+        className="mt-4 mb-4 flex items-center justify-center"
+      >
+        <button
+          id="Calendardownload"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+          onClick={downloadICS}
+        >
+          Kalender-Eintrag herunterladen
+        </button>
+      </div>
     </div>
-    <div id="DownloadDivCalendar" className="mt-4 mb-4 flex items-center justify-center">
-    <button id="Calendardownload" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={downloadICS}>Kalender-Eintrag herunterladen</button>
-  </div>
-  </div>
   );
 }
-function renderEventContent(eventInfo: any) {
-  return (
-    <div>
-      <strong>{eventInfo.event.title}</strong>
-      <p>
-        {eventInfo.event.extendedProps.description}
-      </p>
-    </div>
-  );
-};
-
