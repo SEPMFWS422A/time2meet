6adc18373b2176661c0d32052e0d054e01c0f47b
Christian <Christian.Huwendiek@edu.fhdw.de>
2025-01-16 14:26:22 +0100

    #6 Add fullcalendar
    We added the fullcalendar libary
    
    Finn: 33,3
    Mohamed: 33,3
    Chris: 33,3

---
+++ src/app/lib/models/event.ts
@@ -0,0 +1,8 @@
+export type Event={
+    title: string;
+    description: string;
+    date: Date;
+    startTime: string;
+    endTime:string;
+
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,52 @@
-"use client"
-import {Calendar} from "@nextui-org/react";
-import {parseDate} from "@internationalized/date";
+"use client";
+
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
 
 export default function Home() {
+  const handleDateClick = (arg: any) => {
+    console.log(arg)
+    alert(arg);
+  };
+  const exampleEvents = [
+    {
+      title: "Event 1",
+      start: new Date(2025,0,21).toISOString().split("T")[0],
+      end: new Date(2025,0,24).toISOString().split("T")[0],
+      description: "Termin beim Zahnarzt um 17:00 Uhr",
+    },
+    {
+      title: "Event 2",
+      start: "20250116T130000",
+      duration: { hours: 1, minutes: 30 },
+      description: "Termin beim Zahnarzt um 14:00 Uhr",
+    },
+  ];
+
   return (
-    <div className="flex gap-x-4">
-      <Calendar id="e2eCalendar" aria-label="Date (Uncontrolled)" defaultValue={parseDate(new Date().toISOString().split('T')[0])} />
+    <div className="ml-8 mr-8">
+      <FullCalendar height={"100vh"}
+        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin,listPlugin]}
+        headerToolbar ={{left: "prev,next today", center:"title", right:"dayGridMonth,timeGridWeek,timeGridDay,listWeek"}}
+        dateClick={handleDateClick}
+        events={exampleEvents}
+        eventContent={renderEventContent}
+        initialView="dayGridMonth"
+      />
     </div>
   );
 }
+function renderEventContent(eventInfo: any) {
+  return (
+    <div>
+      <strong>{eventInfo.event.title}</strong>
+      <p>
+        {eventInfo.event.extendedProps.description}
+      </p>
+    </div>
+  );
+};
+
