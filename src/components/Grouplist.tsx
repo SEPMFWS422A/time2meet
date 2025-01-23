c75b37465ccbbd550c11ec9ddda3309482878264
Finn <finnkatzenberger@gmail.com>
2025-01-23 01:02:52 +0100

    Added first design for grouplist ui

---
+++ src/components/Grouplist.tsx
@@ -0,0 +1,43 @@
+import {Avatar, Listbox, ListboxItem} from "@heroui/react";
+
+function Grouplist() {
+    const groups = [
+        {
+            groupName: "Fußball-Gruppe",
+            description: "Beschreibung Fußball",
+        },
+        {
+            groupName: "Thailand-Gruppe",
+            description: "Beschreibung Thailand",
+        },
+        {
+            groupName: "Schminken-Gruppe",
+            description: "Beschreibung Schminken",
+        },
+        {
+            groupName: "Sauf-Gruppe",
+            description: "Beschreibung Saufen",
+        },
+    ];
+
+    return (
+        <Listbox aria-label="Gruppen" variant="flat" items={groups} onAction={(key) => console.log(key)}>
+            {(item) => (
+                <ListboxItem
+                    showDivider
+                    key={item.groupName}
+                >
+                    <div className="flex gap-2 items-center w-screen">
+                        <Avatar alt={item.groupName} size="sm"/>
+                        <div className="">
+                            <span>{item.groupName}</span>
+                        </div>
+                    </div>
+                </ListboxItem>
+            )}
+
+        </Listbox>
+    );
+}
+
+export default Grouplist;
