05a8484e254a6fdee15884cc03f3ef658fff3ba3
elmarraki88 <110703630+elmarraki88@users.noreply.github.com>
2025-01-29 10:48:28 +0100

    Ticket #77 Changed time events color to magenta

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,106 +1,106 @@
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list";
-import {useEvents} from "@/lib/data/events";
+import { useEvents } from "@/lib/data/events";
 import deLocale from "@fullcalendar/core/locales/de";
 
 interface CalendarProps {
-    onOpenDate: () => void;
-    onOpenEvent: (info: any) => void;
+  onOpenDate: () => void;
+  onOpenEvent: (info: any) => void;
 }
 
-const Calendar: React.FC<CalendarProps> = ({onOpenDate, onOpenEvent}) => {
-    const calendarRef = useRef<FullCalendar>(null);
+const Calendar: React.FC<CalendarProps> = ({ onOpenDate, onOpenEvent }) => {
+  const calendarRef = useRef<FullCalendar>(null);
 
-    //events aus dem eventsContext rausziehen ~Chris
-    const {events} = useEvents();
+  //events aus dem eventsContext rausziehen ~Chris
+  const { events } = useEvents();
 
-    const handleDateClick = (info: any) => {
-        onOpenDate();
-    };
+  const handleDateClick = (info: any) => {
+    onOpenDate();
+  };
 
-    const handleEventClick = (info: any) => {
-        onOpenEvent({
-            title: info.event.title,
-            start: info.event.start?.toISOString(),
-            end: info.event.end?.toISOString(),
-            allDay: info.event.allDay,
-            extendedProps: {
-                description: info.event.extendedProps.description,
-                location: info.event.extendedProps.location,
-            },
-        });
-    };
+  const handleEventClick = (info: any) => {
+    onOpenEvent({
+      title: info.event.title,
+      start: info.event.start?.toISOString(),
+      end: info.event.end?.toISOString(),
+      allDay: info.event.allDay,
+      extendedProps: {
+        description: info.event.extendedProps.description,
+        location: info.event.extendedProps.location,
+      },
+    });
+  };
 
-    return (
-        <div className="w-screen">
-            <FullCalendar
-                ref={calendarRef}
-                height={"100vh"}
-                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
-                headerToolbar={{
-                    left: "prev,next today",
-                    center: "title",
-                    right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
-                }}
-                initialView="dayGridMonth"
-                selectable={true}
-                editable={true}
-                events={events} //Hier habe ich die Events aus dem EventsContext gezogen und eingefügt ~Chris
-                eventContent={renderEventContent}
-                dateClick={handleDateClick}
-                eventClick={handleEventClick}
-                //eventColor="green"
-                locale={deLocale}
-                eventDidMount={(info) => {
-                    const el = info.el;
-                    if (info.event.allDay) {
-                        el.style.backgroundColor = "blue";
-                        el.style.borderColor = "blue";
-                    } else {
-                        el.style.backgroundColor = "green";
-                        el.style.borderColor = "green";
-                    }
-                    el.style.color = "white";
-                    el.style.fontWeight = "normal";
+  return (
+    <div className="w-screen">
+      <FullCalendar
+        ref={calendarRef}
+        height={"100vh"}
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
+        }}
+        initialView="dayGridMonth"
+        selectable={true}
+        editable={true}
+        events={events} //Hier habe ich die Events aus dem EventsContext gezogen und eingefügt ~Chris
+        eventContent={renderEventContent}
+        dateClick={handleDateClick}
+        eventClick={handleEventClick}
+        //eventColor="green"
+        locale={deLocale}
+        eventDidMount={(info) => {
+          const el = info.el;
+          if (info.event.allDay) {
+            el.style.backgroundColor = "blue";
+            el.style.borderColor = "blue";
+          } else {
+            el.style.backgroundColor = "magenta";
+            el.style.borderColor = "magenta";
+          }
+          el.style.color = "white";
+          el.style.fontWeight = "normal";
 
-                    el.addEventListener("mouseover", () => {
-                        if (info.event.allDay) {
-                            el.style.backgroundColor = "darkblue";
-                            el.style.borderColor = "darkblue";
-                        } else {
-                            el.style.backgroundColor = "darkgreen";
-                            el.style.borderColor = "darkgreen";
-                        }
-                        el.style.fontWeight = "bold";
-                    });
+          el.addEventListener("mouseover", () => {
+            if (info.event.allDay) {
+              el.style.backgroundColor = "darkblue";
+              el.style.borderColor = "darkblue";
+            } else {
+              el.style.backgroundColor = "darkmagenta";
+              el.style.borderColor = "darkmagenta";
+            }
+            el.style.fontWeight = "bold";
+          });
 
-                    el.addEventListener("mouseout", () => {
-                        if (info.event.allDay) {
-                            el.style.backgroundColor = "blue";
-                            el.style.borderColor = "blue";
-                        } else {
-                            el.style.backgroundColor = "green";
-                            el.style.borderColor = "green";
-                        }
-                        el.style.fontWeight = "normal";
-                    });
-                }}
-            />
-        </div>
-    );
+          el.addEventListener("mouseout", () => {
+            if (info.event.allDay) {
+              el.style.backgroundColor = "blue";
+              el.style.borderColor = "blue";
+            } else {
+              el.style.backgroundColor = "magenta";
+              el.style.borderColor = "magenta";
+            }
+            el.style.fontWeight = "normal";
+          });
+        }}
+      />
+    </div>
+  );
 };
 
 function renderEventContent(eventInfo: any) {
-    return (
-        <div>
-            <strong>{eventInfo.event.title}</strong>
-            <p>{eventInfo.event.extendedProps.description}</p>
-        </div>
-    );
+  return (
+    <div>
+      <strong>{eventInfo.event.title}</strong>
+      <p>{eventInfo.event.extendedProps.description}</p>
+    </div>
+  );
 }
 
 export default Calendar;
