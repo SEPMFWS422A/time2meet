6a89d5b12a9124f153f48cb8456e8ea276575818
istmnn <ili@staimann.de>
2025-01-29 16:03:53 +0100

    Ticket#87 Das Form wird nun bei dem Drücken des entsprechenden Button im Vordergrund aufgerufen

--- src/components/CreateSurvey.tsx
+++ src/components/CreateSurvey.tsx
@@ -1,6 +1,8 @@
+
 import React, {useState} from "react";
-import {Form, Input, Button, Card, CardBody, CardHeader,} from "@heroui/react";
+import {Form, Input, Button, Card, CardBody, CardHeader, Modal,} from "@heroui/react";
 import {CardTitle} from "@/components/ui/card";
+import ModalWindow from "@/components/ModalWindow";
 
 export default function CreateSurvey() {
         const [inputs, setInputs] = useState([{ id: Date.now(), value: '' }]);
@@ -17,50 +19,66 @@ export default function CreateSurvey() {
             setInputs([...inputs, { id: Date.now(), value: '' }]);
         };
 
+
         const handleSubmit = (e) => {
             e.preventDefault();
             const values = inputs.map((input) => input.value);
             console.log(values); // Array mit allen eingegebenen Werten
-        };
+        }
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleOpenModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
 
         return (
-            <Card className="w-full max-w-2xl shadow-xl">
-                <CardHeader className="text-center bg-blue-600 text-white py-4 rounded-t-lg">
-                    <CardTitle className="text-2xl font-bold"> Umfrage erstellen </CardTitle>
-                </CardHeader>
-                <CardBody className="p-6 bg-white">
-                    <Form onSubmit={handleSubmit}>
-                        <Input
-                            isRequired
-                            label="Titel"
-                            labelPlacement="outside"
-                            name="Titel"
-                            placeholder="Titel der Umfrage angeben"
-                            type="title"
-                        />
-                        <Input
-                            label="Beschreibung"
-                            labelPlacement="outside"
-                            name="description"
-                            placeholder="Beschreibung angeben"
-                            type="descr"
-                        />
-                        <div>
-                            Optionen:
-                            {inputs.map((input) => (
+
+            <div className="text-center text-white py-4 rounded-t-lg ">
+                <Button
+                    className="h-10 text-xl bg-blue-600 text-white"
+                    onClick={handleOpenModal}>Umfrage erstellen</Button>
+                <ModalWindow isOpen={isModalOpen} onOpenChange={handleCloseModal} content={
+                            <Form onSubmit={handleSubmit}>
                                 <Input
                                     isRequired
-                                    key={input.id}
-                                    value={input.value}
-                                    onChange={(e) => handleInputChange(e, input.id)}
-                                    placeholder="Gib deinen Text ein"
+                                    label="Titel"
+                                    labelPlacement="outside"
+                                    name="Titel"
+                                    placeholder="Titel der Umfrage angeben"
+                                    type="title"
                                 />
-                            ))}
-                            <Button onClick={handleAddInput}>Option hinzufügen</Button>
-                        </div>
-                        <Button type="submit">Absenden</Button>
-                    </Form>
-                </CardBody>
-            </Card>
+                                <Input
+                                    label="Beschreibung"
+                                    labelPlacement="outside"
+                                    name="description"
+                                    placeholder="Beschreibung angeben"
+                                    type="descr"
+                                />
+                                <div>
+                                    Optionen:
+                                    {inputs.map((input) => (
+                                        <Input
+                                            isRequired
+                                            key={input.id}
+                                            value={input.value}
+                                            onChange={(e) => handleInputChange(e, input.id)}
+                                            placeholder="Gib deinen Text ein"
+                                        />
+                                    ))}
+                                    <Button onClick={handleAddInput}>Option hinzufügen</Button>
+                                </div>
+                                <Button type="submit">Absenden</Button>
+                            </Form>
+
+                } title={'Umfrage erstellen'}>
+
+                </ModalWindow>
+
+            </div>
+
         );
     }
