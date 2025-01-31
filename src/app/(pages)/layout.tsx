21dc9aada899638a545bbb83ca0b1090e743a28f
Finn <finnkatzenberger@gmail.com>
2025-01-31 01:34:25 +0100

    Changed layout files, so that there are not two html and body tags

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,37 +1,18 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
 import "@/app/globals.css";
 import Navbar from "@/components/Navbar";
-
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
     title: "Time2Meet",
 };
 
-export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default function Layout({children}: Readonly<{ children: React.ReactNode }>) {
     return (
-        <html lang="en">
-        <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-        <Navbar/>
-        <div className="flex flex-col w-full">
-            <div>{children}</div>
+        <div>
+            <div>
+                <Navbar/>
+            </div>
+            <div className="flex flex-col w-full">{children}</div>
         </div>
-        </body>
-        </html>
     );
 }
