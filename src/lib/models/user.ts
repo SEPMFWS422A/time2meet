649e2c84a5f7e765957746b3362efb1af70cbf19
Kursat <75574588+DKursat@users.noreply.github.com>
2025-02-02 14:13:57 +0100

    Ticket #97
    update scheme to users
    
    Credits an @TimoNeuhaus
    
    Co-Authored-By: TimoNeuhaus <113421494+TimoNeuhaus@users.noreply.github.com>
    Co-Authored-By: elmarraki88 <110703630+elmarraki88@users.noreply.github.com>

--- src/lib/models/user.ts
+++ src/lib/models/user.ts
@@ -9,7 +9,7 @@ const UserSchema = new mongoose.Schema({
   password: { type: String, required: true },
   telefonnummer: { type: String, default: null },
   geburtsdatum: { type: Date, default: null },
-  freunde: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  freunde: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
   profilsichtbarkeit: {type: String, enum: ["öffentlich", "Privat", "Nur freunde"],default: "öffentlich"},
   kalendersichtbarkeit: {type: String, enum: ["öffentlich", "Privat", "Nur freunde"],default: "öffentlich"},
   theme: {type: String, enum: ["Hell", "dunkel"],default: "Hell"},
