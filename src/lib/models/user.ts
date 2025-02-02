af0367e3328deb7a150d73014db881de21615f8b
Kursat <75574588+DKursat@users.noreply.github.com>
2025-02-02 13:55:26 +0100

    Ticket #97
    Add Mongoose
    Add Scheme for User
    
    33.3%/33.3%/33.3% @elmarraki88 @TimoNeuhaus @Dkursat
    
    Co-Authored-By: TimoNeuhaus <113421494+TimoNeuhaus@users.noreply.github.com>
    Co-Authored-By: elmarraki88 <110703630+elmarraki88@users.noreply.github.com>

---
+++ src/lib/models/user.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  user_id: { type: Number, unique: true, required: true },
+  name: { type: String, required: true },
+  vorname: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  benutzername: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  telefonnummer: { type: String, default: null },
+  geburtsdatum: { type: Date, default: null },
+  freunde: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  profilsichtbarkeit: {type: String, enum: ["öffentlich", "Privat", "Nur freunde"],default: "öffentlich"},
+  kalendersichtbarkeit: {type: String, enum: ["öffentlich", "Privat", "Nur freunde"],default: "öffentlich"},
+  theme: {type: String, enum: ["Hell", "dunkel"],default: "Hell"},
+  profilbild: { type: String, default: null },
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.models.User || mongoose.model("users", UserSchema);
