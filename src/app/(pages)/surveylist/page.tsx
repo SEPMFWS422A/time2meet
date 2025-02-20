094a114a74988f1ddf33e0be5acd424f0870e420
istmnn <ili@staimann.de>
2025-02-20 15:18:21 +0100

    Ticket#120 Umfrage zur Terminfindung (noch statisch)

--- src/app/(pages)/surveylist/page.tsx
+++ src/app/(pages)/surveylist/page.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import MultipleChoiceSurvey from "@/components/MultipleChoiceSurvey";
 import CreateSurvey from "@/components/CreateSurvey";
+import DateSurvey from "@/components/DateSurvey";
 
 export default function About() {
 
@@ -17,6 +18,18 @@ export default function About() {
                     title="Meine Umfrage"
                     description="Eine coole Umfrage"/>
             </div>
+            <DateSurvey
+                title="Terminfindung für das nächste Teammeeting"
+                description="Bitte geben Sie Ihre Präferenzen für die folgenden Termine an."
+                timeSlots={{
+                    '2024-05-10': ['10:00 -11:00', '11:00-12:00', '12:00-13:00'],
+                    '2024-05-11': ['14:00-15:00', '15:00-16:00'],
+                    '2024-05-12': ['16:00-17:00', '17:00-18:00', '18:00-19:00'],
+                }}
+            />
+
+
+
         </div>
     )
 }
