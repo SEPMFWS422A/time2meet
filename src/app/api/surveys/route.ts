1826c6098600163afa6a194cdc20c0aefa2699e9
Christian <Christian.Huwendiek@edu.fhdw.de>
2025-03-09 20:23:11 +0100

    #131 Implement Post Survey Endpoint
    -Implement POST endpoint for creating a survey
    -Add input validation for survey creation using Joi

---
+++ src/app/api/surveys/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+import Survey from "@/lib/models/Survey";
+import dbConnect from "@/lib/database/dbConnect";
+import { getUserID } from "@/lib/helper";
+import { surveyJoiValidationSchema } from "@/lib/validation/surveyValidation";
+
+// POST /api/survey - Neue Umfrage erstellen
+export async function POST(req: NextRequest) {
+    await dbConnect();
+
+    const currentUser = await getUserID(req);
+    if (currentUser.error) {
+        return NextResponse.json({ message: currentUser.error }, { status: currentUser.status });
+    }
+
+    try {
+        const body = await req.json();
+        // Validation mit Joi siehe: @/lib/validation/surveyValidation"
+    const { error, value } = surveyJoiValidationSchema.validate(body);
+    if (error) {
+      return NextResponse.json(
+        { message: 'Validierungsfehler', details: error.details },
+        { status: 400 }
+      );
+    }
+
+    //Wenn die Validation erfolgreich war und keine Fehler zurück kommt, werden die Values übernommen und eine neue Survey wird erstellt
+    const newSurvey = new Survey({
+        ...value,
+        creator: currentUser.id,
+        //Creator immer als Participant einfügen
+        participants: value.participants 
+          ? [...new Set([...value.participants, currentUser.id])]
+          : [currentUser.id]
+      });
+
+        await newSurvey.save();
+
+        return NextResponse.json({ message: "Umfrage erfolgreich erstellt", survey: newSurvey }, { status: 201 });
+    } catch (error: any) {
+        return NextResponse.json({ message: "Interner Serverfehler", error: error.message }, { status: 500 });
+    }
+}
