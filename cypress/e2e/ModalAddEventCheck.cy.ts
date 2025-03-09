27d4279687b570b886c8e99f8daef85a4e9bbf7e
Christian <Christian.Huwendiek@edu.fhdw.de>
2025-03-09 23:38:42 +0100

    #60 Updated E2E Tests
    I Updated the E2E tests for the AddEventModal.

--- cypress/e2e/ModalAddEventCheck.cy.ts
+++ cypress/e2e/ModalAddEventCheck.cy.ts
@@ -1,6 +1,12 @@
 describe('AddModalContentCheck', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000');
+
+        cy.get('input#email').type('K2@K2.com');
+        cy.get('input#password').type('123');
+        cy.contains('button', 'Anmelden').click();
+        cy.url().should('eq', 'http://localhost:3000/');
+
         cy.get('td.fc-day').not('.fc-event').first().click();
         cy.get('section[role="dialog"]').should('be.visible');
     });
@@ -9,8 +15,8 @@ describe('AddModalContentCheck', () => {
         cy.get('section[role="dialog"]').should('be.visible');
         cy.get('section[role="dialog"] header').should('have.text', 'Event hinzufügen');
         cy.get('#addEventForm').should('exist');
-        cy.get('input[placeholder="Geben Sie den Titel des Ereignisses ein"]').should('be.visible');
-        cy.get('input[placeholder="Geben Sie die Beschreibung des Ereignisses ein"]').should('be.visible');
+        cy.get('#title').should('be.visible');
+        cy.get('#description').should('be.visible');
         cy.get('input[placeholder="Wählen Sie Startdatum und -uhrzeit"]').should('be.visible');
         cy.get('input[placeholder="Geben Sie den Ort ein"]').should('be.visible');
         cy.get('button').contains('Schließen').should('be.visible');
@@ -22,36 +28,36 @@ describe('AddModalContentCheck', () => {
         cy.get('button').contains('Ereignis speichern').click(); 
         cy.get('#generalFormError').should('be.visible');
 
-        cy.get('input[placeholder="Geben Sie den Titel des Ereignisses ein"]').type('Test Event');
+        cy.get('#title').type('Test Event');
         cy.get('button').contains('Ereignis speichern').click();
         cy.get('#generalFormError').should('be.visible');
 
-        cy.get('#generalFormError').should('contain', 'Überprüfen Sie Ihre Eingaben');
+        cy.get('#generalFormError').should('contain', 'Bitte alle erforderlichen Felder ausfüllen.');
     });
 
     it('should save an all-day event', () => {
         cy.get('section[role="dialog"]').should('be.visible');
-        cy.get('input[placeholder="Geben Sie den Titel des Ereignisses ein"]').type('Test All-Day Event');
-        cy.get('input[placeholder="Geben Sie die Beschreibung des Ereignisses ein"]').type('Test description');
-        cy.get('input[placeholder="Wählen Sie Startdatum und -uhrzeit"]').type('2025-01-22T10:00');
-        cy.get('input[placeholder="Geben Sie den Ort ein"]').type('Test location');
         cy.get('input[type="checkbox"]').check(); 
+        cy.get('#title').type('Test All-Day Event');
+        cy.get('#description').type('Test description');
+        cy.get('input[placeholder="Wählen Sie das Startdatum"]').type('2025-03-22');
+        cy.get('input[placeholder="Geben Sie den Ort ein"]').type('Test location');
       
         cy.get('button').contains('Ereignis speichern').should('not.be.disabled'); 
         cy.get('button').contains('Ereignis speichern').click();
       
         cy.get('section[role="dialog"]').should('not.exist');
 
-        cy.get('#HomePageLayout').find('td[data-date="2025-01-22"]').find('a.fc-event').should('have.class', 'fc-event').and('include.text', 'Test All-Day Event');
+        cy.get('#HomePageLayout').find('td[data-date="2025-03-22"]').find('a.fc-event').should('have.class', 'fc-event').and('include.text', 'Test All-Day Event');
       });
 
     it('should save an event with start and end time', () => {
         cy.get('section[role="dialog"]').should('be.visible');
       
-        cy.get('input[placeholder="Geben Sie den Titel des Ereignisses ein"]').type('Test normal Event');
-        cy.get('input[placeholder="Geben Sie die Beschreibung des Ereignisses ein"]').type('Test description');
-        cy.get('input[placeholder="Wählen Sie Startdatum und -uhrzeit"]').type('2025-01-22T10:00');
-        cy.get('input[placeholder="Wählen Sie Enddatum und -uhrzeit"]').type('2025-01-22T12:00');
+        cy.get('#title').type('Test normal Event');
+        cy.get('#description').type('Test description');
+        cy.get('input[placeholder="Wählen Sie Startdatum und -uhrzeit"]').type('2025-03-22T10:00');
+        cy.get('input[placeholder="Wählen Sie Enddatum und -uhrzeit"]').type('2025-03-22T12:00');
         cy.get('input[placeholder="Geben Sie den Ort ein"]').type('Test location');
       
         cy.get('button').contains('Ereignis speichern').should('not.be.disabled');
@@ -59,18 +65,18 @@ describe('AddModalContentCheck', () => {
       
         cy.get('section[role="dialog"]').should('not.exist');
 
-        cy.get('#HomePageLayout').find('td[data-date="2025-01-22"]').find('a.fc-event').should('have.class', 'fc-event').and('include.text', 'Test normal Event');
+        cy.get('#HomePageLayout').find('td[data-date="2025-03-22"]').find('a.fc-event').should('have.class', 'fc-event').and('include.text', 'Test normal Event');
     });
 
     it('should show invalid input for "Endtime" when "Endtime" is in the past compared to "Starttime" ', () => {
         cy.get('section[role="dialog"]').should('be.visible');
       
-        cy.get('input[placeholder="Geben Sie den Titel des Ereignisses ein"]').type('Test normal Event');
-        cy.get('input[placeholder="Wählen Sie Startdatum und -uhrzeit"]').type('2025-01-22T10:00');
-        cy.get('input[placeholder="Wählen Sie Enddatum und -uhrzeit"]').type('2025-01-21T12:00');
+        cy.get('#title').type('Test normal Event');
+        cy.get('input[placeholder="Wählen Sie Startdatum und -uhrzeit"]').type('2025-03-22T10:00');
+        cy.get('input[placeholder="Wählen Sie Enddatum und -uhrzeit"]').type('2025-03-21T12:00');
 
         cy.get('div[data-invalid="true"]').should('contain', 'Ungültige Endzeit');    
         cy.get('button').contains('Ereignis speichern').click();
-        cy.get('#generalFormError').should('be.visible').should('contain', 'Überprüfen Sie Ihre Eingaben'); 
+        cy.get('#generalFormError').should('be.visible').should('contain', 'Bitte alle erforderlichen Felder ausfüllen.'); 
     });
 });
